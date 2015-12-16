49bb6cda56cb34902b92eed48fd94b626bd538d5
adow <reynoldqin@gmail.com>
2015-12-16 13:39:22 +0800

    Initial Commit

---
+++ SecrecySwift/SecrecySwift.h
@@ -0,0 +1,19 @@
+//
+//  SecrecySwift.h
+//  SecrecySwift
+//
+//  Created by 秦 道平 on 15/12/16.
+//  Copyright © 2015年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecrecySwift.
+FOUNDATION_EXPORT double SecrecySwiftVersionNumber;
+
+//! Project version string for SecrecySwift.
+FOUNDATION_EXPORT const unsigned char SecrecySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecrecySwift/PublicHeader.h>
+
+
